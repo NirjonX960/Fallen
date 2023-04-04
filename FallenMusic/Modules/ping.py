f21998a29f4ff689dd38cb20db62c1db57759e3c
NirjonX960 <129636950+NirjonX960@users.noreply.github.com>
2023-04-04 20:25:10 +0600

    Update ping.py

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -59,7 +59,7 @@ async def ping_fallen(_, message: Message):
                 [
                     InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
-                        "✨ sᴏᴜʀᴄᴇ ✨",
+                        "✨ ᴜᴘᴅᴀᴛᴇ ✨",
                         url="https://t.me/NixaWorld",
                     ),
                 ],
